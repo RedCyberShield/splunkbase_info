7ceb0bd12457c84253eb73b291217d9b8bec95e3
Red Cyber Shield <josh@redcybershield.com>
2021-07-28 11:00:29 -0400

    Update splunkbase_info.py

--- splunkbase_info.py
+++ splunkbase_info.py
@@ -3,27 +3,36 @@ import re
 import sys 
 import os 
 
+
+## DEF CREATION
 def find(substr, infile, outfile):
     with open(infile) as a, open(outfile, 'a') as b:
+        b.write('\n' + '\n' + infile + '\n')
         for line in a:
-            b.write('\n' + '\n' + infile + '\n')
-            if substr in line:
-                b.write(line + '\n') 
+            for find in substr:
+                if find in line:
+                    b.write(line + '\n') 
 
+
+# CREATE OUTPUT FILE
+file1 = open("splunkbase_info_output.txt", "w")  # write mode
+file1.write("Splunk App & TA Info \n" + '\n' + '\n')
+file1.close()
+
+
+## VAR CREATION
 file_path = []
+substr = ['label','id','version']
+
 
 ## CREATE FILE LIST
 for file_name in glob.iglob('./**/app.conf', recursive=True):
     file_path.append(file_name)
-
 print(file_path)
+
 
 ## ITERATE THROUGH FILE LIST TO FIND STRINGS
 for file in file_path:
     print(file)
-    find('label', file, 'splunkbase_info_output.txt')
-    find('id', file, 'splunkbase_info_output.txt')
-    find('version', file, 'splunkbase_info_output.txt')
-    # print(file.startswith("label"))
-    # print(file.startswith("id"))
-    # print(file.startswith("version"))
+    find(substr, file, 'splunkbase_info_output.txt')
+
